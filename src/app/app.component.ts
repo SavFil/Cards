a00e6219a8a1d33e057aff9dea33ad4ec7565020
Savvas Filippidis <filippides.savvas@gmail.com>
2021-03-28 15:10:23 +0300

    Finished card Tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/img1.jpg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/img2.jpg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/img3.jpg',
+      username: 'biking12222',
+      content: 'I like biking'
+    },
+    {
+      title: 'Hawaii 5 - 0',
+      imageUrl: 'assets/img3.jpg',
+      username: 'ICopiedYourPhoto',
+      content: 'You mad bruh?'
+    }
+  ];
 }
